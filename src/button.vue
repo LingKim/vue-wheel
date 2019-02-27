413c17fc36a697a90058d595b19a139e91aba763
lilin <1660312954@qq.com>
2019-02-27 21:17:46 +0800

    button组件样式完成

--- src/button.vue
+++ src/button.vue
@@ -1,5 +1,5 @@
 <template>
-    
+    <button class="g-button">按钮</button>
 </template>
 
 <script>
@@ -8,6 +8,22 @@
 	}
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    .g-button {
+       font-size:var(--font-size);
+       height: var(--button-height);
+       padding: 0 1em;
+       border-radius: var(--border-radius);
+       border: 1px solid var(--border-color);
+       background: var(--button-bg);
+       &:hover {
+           border-color: var(--border-color-hover);
+       }
+       &:active {
+           background-color: var(--button-active-bg);
+       }
+       &:focus {
+           outline: none;
+       }
+    }
 </style>
