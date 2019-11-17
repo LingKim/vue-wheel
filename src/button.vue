efdf6f658c159398620bdb7c44b28b0ef25a5c2d
lilin <1660312954@qq.com>
2019-11-17 21:40:03 +0800

    按钮组件添加icon

--- src/button.vue
+++ src/button.vue
@@ -1,29 +1,83 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <g-icon class="icon loading" v-if="icon" :name="icon"></g-icon>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
 	export default {
-		name: "button"
+		name: "button",
+		props: {
+			icon: {},
+			iconPosition: {
+				type: String,
+				default: 'left',
+				validator(value) {
+					return value === 'left' || value === 'right';
+				}
+			}
+		}
 	}
 </script>
 
 <style lang="scss" scoped>
+    @keyframes spin{
+        0% {
+            transform: rotate(0);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+    .loading {
+        animation: spin 2s infinite linear;
+    }
     .g-button {
-       font-size:var(--font-size);
-       height: var(--button-height);
-       padding: 0 1em;
-       border-radius: var(--border-radius);
-       border: 1px solid var(--border-color);
-       background: var(--button-bg);
-       &:hover {
-           border-color: var(--border-color-hover);
-       }
-       &:active {
-           background-color: var(--button-active-bg);
-       }
-       &:focus {
-           outline: none;
-       }
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-content: center;
+        vertical-align: middle;
+
+        > .content {
+            order: 2
+        }
+
+        > .icon {
+            order: 1;
+            margin-right: .1em;
+        }
+
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+
+        &:focus {
+            outline: none;
+        }
+
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: .1em;
+            }
+
+            > .content {
+                order: 1
+            }
+        }
     }
 </style>
---
+++ src/icon.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg class="g-icon">
+        <use :xlink:href=`#i-${name}`></use>
+    </svg>
+</template>
+
+<script>
+	export default {
+		name: "icon",
+        props:['name']
+	}
+</script>
+
+<style lang="scss" scoped>
+    .g-icon {
+        width: 1.5em;
+        height: 1.5em;
+    }
+</style>
