fb7b8913efc0fa8c531a9e3da953bd957f898597
lilin <1660312954@qq.com>
2019-12-01 20:07:30 +0800

    完成row和col组件

--- src/app.js
+++ src/app.js
@@ -3,17 +3,22 @@ import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group.vue'
 import Input from './Input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('g-button',Button);
 Vue.component('g-icon',Icon);
 Vue.component('g-button-group',ButtonGroup);
 Vue.component('g-input',Input);
+Vue.component('g-row', Row);
+Vue.component('g-col', Col);
 
 new Vue({
   el:'#app',
 	data() {
   	return {
-		  loading1:true
+		  loading1:true,
+		  message:''
 	  }
 	},
 	methods: {
